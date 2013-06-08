2b4b05ab20088a9cce36c2345ece9ed7514b6f53
邓建平 <tinydjp@gmail.com>
2013-06-08 21:25:40 +0800

    配置了htaccess，加入了第一个测试的controller:home
    稍微研究了下controller的编写模式，下一步转入modules和views

---
+++ application/controllers/home.php
@@ -0,0 +1,35 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+/**
+ * Created by JetBrains PhpStorm.
+ * User: Stone_Giant
+ * Date: 13-6-8
+ * Time: 下午8:06
+ * To change this template use File | Settings | File Templates.
+ */
+
+class Home extends CI_Controller {
+    /**
+     * 重载的构造函数，可不写，在需要初始化时有用
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * 默认的index方法
+     */
+    public function index()
+    {
+        echo 'Hello World!';
+    }
+
+    /**
+     * 重载CI_Controller中的方法，在最后被调用
+     * @param $output
+     */
+    public function _output($output)
+    {
+        echo $output;//'This is a overrided method'.(is_null($output));
+    }
+}
